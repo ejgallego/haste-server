dc0fbfd577be58aabdbdc5f55df9e9e945bbe686
Emilio Jesus Gallego Arias <e+git@x80.org>
2016-03-02 23:52:00 +0100

    Sound logic for reset and coq.

--- static/coq-app.js
+++ static/coq-app.js
@@ -1,17 +1,22 @@
 ///// represents a single document
 
+// Saved means that we can play but not save until we explicitely fork.
 var haste_document = function() {
-  this.locked = false;
+  this.saved = false;
 };
 
 // Get this document from the server and lock it here
 haste_document.prototype.load = function(key, callback, lang) {
+
   var _this = this;
+
   $.ajax('/documents/' + key, {
     type: 'get',
     dataType: 'json',
+
     success: function(res) {
-      _this.locked = true;
+
+      _this.saved = true;
       _this.key = key;
       _this.data = res.data;
 
@@ -21,6 +26,7 @@ haste_document.prototype.load = function(key, callback, lang) {
         language: 'coq',
       });
     },
+
     error: function(err) {
       callback(false);
     }
@@ -29,23 +35,25 @@ haste_document.prototype.load = function(key, callback, lang) {
 
 // Save this document to the server and lock it here
 haste_document.prototype.save = function(data, callback) {
-  if (this.locked) {
+
+  if (this.saved) {
     return false;
   }
+
   this.data = data;
   var _this = this;
+
   $.ajax('/documents', {
     type: 'post',
     data: data,
     dataType: 'json',
     contentType: 'application/json; charset=utf-8',
     success: function(res) {
-      _this.locked = true;
+      _this.saved = true;
       _this.key = res.key;
-      // var high = hljs.highlightAuto(data);
-      var high = data;
+
       callback(null, {
-        value: high,
+        value: data,
         key: res.key,
         language: 'coq',
       });
@@ -65,9 +73,11 @@ haste_document.prototype.save = function(data, callback) {
 
 var haste = function(appName, options) {
   this.appName = appName;
-  // this.$textarea = $('textarea');
-  // this.$editor = $('#code-wrapper').first();
+  // A codeMirror instance.
+
+  this.$coq    = null;
   this.$editor = null;
+
   this.options = options;
   this.configureShortcuts();
   this.configureButtons();
@@ -104,6 +114,7 @@ haste.prototype.fullKey = function() {
 
 // Set the key up for certain things to be enabled
 haste.prototype.configureKey = function(enable) {
+
   var $this, i = 0;
   $('#box2 .function').each(function() {
     $this = $(this);
@@ -129,8 +140,11 @@ haste.prototype.newDocument = function(hideHistory) {
 
   this.setTitle();
   this.lightKey();
+
+  // New document: we reset coq.
   this.$editor.setValue('');
   this.$editor.focus();
+  this.$coq.reset();
 };
 
 // Map of common extensions
@@ -163,17 +177,22 @@ haste.prototype.lookupTypeByExtension = function(ext) {
 
 // Load a document and show it
 haste.prototype.loadDocument = function(key) {
+
   // Split the key up
   var parts = key.split('.', 2);
+
   // Ask for what we want
   var _this = this;
+
   _this.doc = new haste_document();
   _this.doc.load(parts[0], function(ret) {
     if (ret) {
       _this.setTitle(ret.key);
       _this.fullKey();
+
       _this.$editor.setValue(ret.value);
       _this.$editor.focus();
+      _this.$coq.reset();
     }
     else {
       _this.newDocument();
@@ -181,31 +200,37 @@ haste.prototype.loadDocument = function(key) {
   }, this.lookupTypeByExtension(parts[1]));
 };
 
-// Duplicate the current document - only if locked
+// Duplicate the current document - only if already saved
 haste.prototype.duplicateDocument = function() {
-  if (this.doc.locked) {
+
+  if (this.doc.saved) {
     var currentData = this.doc.data;
+
     this.newDocument();
-    // this.$textarea.val(currentData);
+
     this.$editor.setValue(currentData);
+    this.$editor.focus();
   }
 };
 
 // Lock the current document
 haste.prototype.lockDocument = function() {
+
   var _this = this;
-  // this.doc.save(this.$textarea.val(), function(err, ret) {
+
   this.doc.save(this.$editor.getValue(), function(err, ret) {
     if (err) {
       _this.showMessage(err.message, 'error');
     }
     else if (ret) {
-      // _this.$code.html(ret.value);
+
       _this.setTitle(ret.key);
+
       var file = '/' + ret.key;
       if (ret.language) {
         file += '.' + _this.lookupExtensionByType(ret.language);
       }
+
       window.history.pushState(null, _this.appName + '-' + ret.key, file);
       _this.fullKey();
     }
@@ -244,7 +269,7 @@ haste.prototype.configureButtons = function() {
       $where: $('#box2 .duplicate'),
       label: 'Duplicate & Edit',
       shortcut: function(evt) {
-        return _this.doc.locked && evt.ctrlKey && evt.keyCode === 68;
+        return _this.doc.saved && evt.ctrlKey && evt.keyCode === 68;
       },
       shortcutDescription: 'control + d',
       action: function() {
@@ -266,7 +291,7 @@ haste.prototype.configureButtons = function() {
       $where: $('#box2 .twitter'),
       label: 'Twitter',
       shortcut: function(evt) {
-        return _this.options.twitter && _this.doc.locked && evt.shiftKey && evt.ctrlKey && evt.keyCode == 84;
+        return _this.options.twitter && _this.doc.saved && evt.shiftKey && evt.ctrlKey && evt.keyCode == 84;
       },
       shortcutDescription: 'control + shift + t',
       action: function() {
@@ -316,40 +341,3 @@ haste.prototype.configureShortcuts = function() {
     }
   });
 };
-
-///// Tab behavior in the textarea - 2 spaces per tab
-$(function() {
-
-/*
-  $('textarea').keydown(function(evt) {
-    if (evt.keyCode === 9) {
-      evt.preventDefault();
-      var myValue = '  ';
-      // http://stackoverflow.com/questions/946534/insert-text-into-textarea-with-jquery
-      // For browsers like Internet Explorer
-      if (document.selection) {
-        this.focus();
-        sel = document.selection.createRange();
-        sel.text = myValue;
-        this.focus();
-      }
-      // Mozilla and Webkit
-      else if (this.selectionStart || this.selectionStart == '0') {
-        var startPos = this.selectionStart;
-        var endPos = this.selectionEnd;
-        var scrollTop = this.scrollTop;
-        this.value = this.value.substring(0, startPos) + myValue +
-          this.value.substring(endPos,this.value.length);
-        this.focus();
-        this.selectionStart = startPos + myValue.length;
-        this.selectionEnd = startPos + myValue.length;
-        this.scrollTop = scrollTop;
-      }
-      else {
-        this.value += myValue;
-        this.focus();
-      }
-    }
-  });
-*/
-});
